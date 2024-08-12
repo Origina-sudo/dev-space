de7c73befc20b6200abf52ef74ae45d4c4370382
Origina-sudo <arthurba8585@gmail.com>
2024-08-12 12:36:51 +0000

    Views

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+def home(request):
+    return HttpResponse('HOme Page')
+
+def room(request):
+    return HttpResponse('Room')
+
 # Create your views here.
--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -3,11 +3,6 @@ from django.urls import path
 from django.http import HttpResponse
 
 
-def home(request):
-    return HttpResponse('HOme Page')
-
-def room(request):
-    return HttpResponse('Room')
 
 
 urlpatterns = [
