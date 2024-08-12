d25eb90298af0ed5ed0444f31ae74e6b0cd967e0
Origina-sudo <arthurba8585@gmail.com>
2024-08-12 13:20:49 +0000

    views

--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
 
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home),
-    path("room/",room)
+    path("", include("base.urls")),
 ]
