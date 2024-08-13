ce725a9aba07f01de14fa1aa1e857e7f9c4a9d22
Origina-sudo <arthurba8585@gmail.com>
2024-08-13 12:00:42 +0000

    Rooms

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -15,5 +15,10 @@ def home(request):
     return render(request,"base/home.html",context )
 
 def room(request,pk):
-    return render(request, "base/room.html")
+    room = None
+    for i in rooms :
+        if i['id]']== int(pk):
+            room = i
+        context={"room":room}
+    return render(request, "base/room.html",context)
 # ,{"rooms" : rooms})
