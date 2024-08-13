b6edb0af0b45b2cec70b7b939e650be48954d05d
Origina-sudo <arthurba8585@gmail.com>
2024-08-13 20:40:21 +0000

    trying to find the buG within the code

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -14,11 +14,11 @@ def home(request):
     context = {"rooms" : rooms}
     return render(request,"base/home.html",context )
 
-def room(request,pk):
+def room(request, pk):
     room = None
     for i in rooms :
-        if i['id]']== int(pk):
+        if i['id]'] == int(pk):
             room = i
-        context={"room":room}
-    return render(request, "base/room.html",context)
+    context={"room": room}
+    return render(request, "base/room.html", context)
 # ,{"rooms" : rooms})
