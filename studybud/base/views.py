0287045f38c5bec935f0b08fa480f646e3960b75
Origina-sudo <arthurba8585@gmail.com>
2024-08-15 17:15:50 +0000

    updates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Room
 
 rooms =[
     {"id":1,"name":"lets Learn Python!"},
@@ -11,6 +11,7 @@ rooms =[
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects
     context = {"rooms" : rooms}
     return render(request,"base/home.html",context )
 
