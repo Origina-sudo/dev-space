724bdc363aac7a9deca10ac68cfa577c29553719
Origina-sudo <arthurba8585@gmail.com>
2024-08-19 13:41:12 +0000

    Connecting Topic to room

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -14,7 +14,7 @@ class Topic(models.Model):
 
 class Room(models.Model):
     # host 
-    # topic =
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True,blank=True)
     # participants
