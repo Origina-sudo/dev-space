79b9e090408d89faa0e0e4c94e565fa8ec0bb9ff
Origina-sudo <arthurba8585@gmail.com>
2024-08-19 13:26:50 +0000

    updates

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Room(models.Model):
     # host 
     # topic =
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render
 from .models import Room
 
-rooms =[
-    {"id":1,"name":"lets Learn Python!"},
-    {"id":2,"name":"Design with me"},
-    {"id":3,"name":"Frontend Developer!"},
-]
+# rooms =[
+#     {"id":1,"name":"lets Learn Python!"},
+#     {"id":2,"name":"Design with me"},
+#     {"id":3,"name":"Frontend Developer!"},
+# ]
 
 
 
 # Create your views here.
 def home(request):
-    # rooms = Room.objects
+    rooms = Room.objects.all()
     context = {"rooms" : rooms}
     return render(request,"base/home.html", context )
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {"room": room}
     return render(request, "base/room.html", context)
