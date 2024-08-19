47e0d03683db6cadb053757957324e300f31129d
Origina-sudo <arthurba8585@gmail.com>
2024-08-19 13:52:44 +0000

    Updates

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User  
+from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
-# Create your models here.
 
+# Create your models here.
 
 
 class Topic(models.Model):
@@ -13,17 +13,17 @@ class Topic(models.Model):
 
 
 class Room(models.Model):
-    # host 
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic= models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True,blank=True)
+    description = models.TextField(null=True, blank=True)
     # participants
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-    
+
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -31,6 +31,6 @@ class Message(models.Model):
     body = models.TextField()
     updated = models.DateTimeField(auto_now_add=True)
     created = models.DateTimeField(auto_now=True)
-     
+
     def __str__(self):
         return self.body[0:50]
