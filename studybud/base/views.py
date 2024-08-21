7144d13e1f3c89d207bb87d7826c6c17703f2273
Origina-sudo <arthurba8585@gmail.com>
2024-08-21 11:15:28 +0000

    MAking dynamic Search possible

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Room , Topic
 from .forms import RoomForm
+from django.db.models import Q
 
 # rooms =[
 #     {"id":1,"name":"lets Learn Python!"},
@@ -13,7 +14,11 @@ from .forms import RoomForm
 # Create your views here.
 def home(request):
     q = request.GET.get('q')  if request.GET.get('q') != None else ""
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
     topics = Topic.objects.all()
     context = {"rooms" : rooms, "topics" : topics}
     return render(request,"base/home.html", context )
