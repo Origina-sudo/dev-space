11f799e5dbde62eaf5077c7ffcdaa3683149db1e
Origina-sudo <arthurba8585@gmail.com>
2024-08-21 10:55:16 +0000

    Updates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -13,7 +13,7 @@ from .forms import RoomForm
 # Create your views here.
 def home(request):
     q = request.GET.get('q')  if request.GET.get('q') != None else ""
-    rooms = Room.objects.filter(topic__name = q)
+    rooms = Room.objects.filter(topic__name__icontains=q)
     topics = Topic.objects.all()
     context = {"rooms" : rooms, "topics" : topics}
     return render(request,"base/home.html", context )
