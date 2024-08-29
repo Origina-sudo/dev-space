4122f25429f0309003ba47a211a807996439ec4a
Origina-sudo <arthurba8585@gmail.com>
2024-08-29 15:50:08 +0000

    Updating Schema

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -4,6 +4,7 @@ from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate , login ,logout
 
 # rooms =[
 #     {"id":1,"name":"lets Learn Python!"},
@@ -21,9 +22,18 @@ def loginPage(request):
         password = request.POST.get("password")
 
         try:
-            user = user.objects.get(username=username)
+            user = User.objects.get(username=username)
         except:
             messages.error(request, "User does not exist")
+
+        user = authenticate(request, username=username , password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        
+        else:
+            messages.error(request, "Username or Password does not exist")
 
     context = {}
     return render(request, "base/login_register.html", context)
