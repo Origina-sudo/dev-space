224d1e720b0a558e6340d6590b6e34a9d11ebbfe
Origina-sudo <arthurba8585@gmail.com>
2024-08-29 15:36:13 +0000

    Updating Code Schema

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render,redirect
-from .models import Room , Topic
+from django.shortcuts import render, redirect
+from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # rooms =[
 #     {"id":1,"name":"lets Learn Python!"},
@@ -10,34 +12,38 @@ from django.db.models import Q
 # ]
 
 
-
 # Create your views here.
 
+
 def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = user.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+
     context = {}
-    return render(request, "base/login_register.html" , context )
-
-
-
+    return render(request, "base/login_register.html", context)
 
 
 def home(request):
-    q = request.GET.get('q')  if request.GET.get('q') != None else ""
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
     rooms = Room.objects.filter(
-        Q(topic__name__icontains=q) |
-        Q(name__icontains=q) |
-        Q(description__icontains=q)
-        )
+        Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)
+    )
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {"rooms" : rooms, "topics" : topics, "room_count" : room_count }
-    return render(request,"base/home.html", context )
+    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
+    return render(request, "base/home.html", context)
+
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {"room": room}
     return render(request, "base/room.html", context)
-
 
 
 def createRoom(request):
@@ -46,24 +52,24 @@ def createRoom(request):
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
-    context = {'form': form}
-    return render(request, "base/room_form.html", context) 
+            return redirect("home")
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
 
 
-
-def updateRoom(request,pk):
+def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-    context = {'form' : form }
-    return render(request, 'base/room_form.html', context)
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
 
 
-
-def deleteRoom(request,pk):
+def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         room.delete()
         return redirect("home")
-    return render(request, 'base/delete.html', {'obj':room})
+    return render(request, "base/delete.html", {"obj": room})
+
+
 #
