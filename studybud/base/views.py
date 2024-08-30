34aab7789814dd04274ffdeb2bb7bdc3fcc62a27
Origina-sudo <arthurba8585@gmail.com>
2024-08-30 17:49:55 +0000

    Return redirect login

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -20,6 +20,9 @@ from django.http import HttpResponse
 
 
 def loginPage(request):
+    if request.user.is_authenticated():
+        return redirect('home')
+
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
