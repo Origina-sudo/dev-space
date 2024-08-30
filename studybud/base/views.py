d5045fe9136af42153298b8a974a1df32b126bc8
Origina-sudo <arthurba8585@gmail.com>
2024-08-30 18:00:42 +0000

    Updates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -48,6 +48,10 @@ def logoutUser(request):
     logout(request)
     return redirect('home')
 
+def registerUser(request):
+    page = 'register'
+    return render(request, "base/login_register.html")
+
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ""
     rooms = Room.objects.filter(
