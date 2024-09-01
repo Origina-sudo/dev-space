9044f001c2be383d434fa6d7c75b2dbad51e1f6a
Origina-sudo <arthurba8585@gmail.com>
2024-09-01 18:08:53 +0000

    Updates

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -17,7 +17,7 @@ class Room(models.Model):
     topic= models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    participants = models.ManyToManyField(User, related_name='paticipants')
+    participants = models.ManyToManyField(User, related_name='paticipants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
